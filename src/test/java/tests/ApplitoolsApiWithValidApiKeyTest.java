a2a13ae99b788b1544fdf728269337470b151391
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2020-03-27 10:40:54 +0100

    try to fix failure in last build

--- src/test/java/tests/ApplitoolsApiWithValidApiKeyTest.java
+++ src/test/java/tests/ApplitoolsApiWithValidApiKeyTest.java
@@ -12,9 +12,7 @@ import util.applitools.ApplitoolsApi;
 
 @Browser("Chrome_headless")
 public class ApplitoolsApiWithValidApiKeyTest extends AbstractTest
-{ // for local test run, please enter your api key here
-    private static final String apiKey = System.getenv("API_KEY");
-
+{
     @After
     public void endAssertions()
     {
@@ -25,7 +23,7 @@ public class ApplitoolsApiWithValidApiKeyTest extends AbstractTest
     public void testOpenEyesWithValidApiKey() throws IOException
     {
         Selenide.open("https://www.xceptance.com/en/");
-        writePropertiy(devPropertiesFilename, "applitools.apiKey", apiKey);
+        writePropertiy(devPropertiesFilename, "applitools.apiKey", System.getenv("API_KEY"));
         writePropertiy(devPropertiesFilename, "applitools.projectName", "Unit-Test");
         ApplitoolsApi.setupGlobal();
         ApplitoolsApi.openEyes("test");
