2d6ffe08f1d9a7c1007622e3ed85f829b6c911fd
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2020-07-21 16:41:07 +0200

    try to delete Credentials class

--- src/test/java/util/applitools/tests/validkey/AbstractValidKeyTest.java
+++ src/test/java/util/applitools/tests/validkey/AbstractValidKeyTest.java
@@ -5,27 +5,25 @@ import org.junit.Before;
 
 import com.xceptance.neodymium.module.statement.browser.multibrowser.Browser;
 
-import util.Credentials;
 import util.applitools.ApplitoolsApi;
 import util.applitools.tests.AbstractCloseEyesAfterTest;
 
 @Browser("Chrome_headless")
 public abstract class AbstractValidKeyTest extends AbstractCloseEyesAfterTest
 {
-    protected static final Credentials CREDENTIALS = ConfigFactory.create(Credentials.class, System.getenv());
+    protected static final String username = System.getenv("USERNAME") == null ? "<PLACE USERNAME HERE>" : System.getenv("USERNAME");
 
-    protected static final String applitoolsApiKey = CREDENTIALS.applitoolsApiKey();
-
-    protected static final String username = CREDENTIALS.username();
-
-    protected static final String password = CREDENTIALS.password();
+    protected static final String password = System.getenv("PASSWORD") == null ? "<PLACE PASSWORD HERE>" : System.getenv("PASSWORD");
 
     protected static final String batchName = "Plugin Unit Tests";
 
     @Before
     public void beforeEach()
     {
-        configProperties.put("applitools.apiKey", applitoolsApiKey);
+        if (System.getenv("APIKEY") != null)
+        {
+            configProperties.put("applitools.apiKey", System.getenv("APIKEY"));
+        }
         configProperties.put("applitools.projectName", "Unit-Test");
         writeMapToPropertiesFile(configProperties, tempConfigFile);
         ConfigFactory.setProperty(ApplitoolsApi.TEMPORARY_CONFIG_FILE_PROPERTY_NAME, "file:" + configFileLocation);
